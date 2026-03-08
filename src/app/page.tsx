a1ae07d2da3828846d591ebb01d2c5bf67955f23
Lili <Liliia_Nel@proton.me>
2026-03-08 20:17:51 +0100

    feat: redirect root to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-      <main>
-          <h1 className="text-2xl">Home page</h1>
-      </main>
-  );
+    redirect('/dashboard');
 }
