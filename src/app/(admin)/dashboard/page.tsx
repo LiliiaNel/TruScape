b6c4ee0a6a740149d23fc38fee5ba4fdedd3f155
Lili <Liliia_Nel@proton.me>
2026-03-08 22:42:24 +0100

    feat: add header to dashboard page

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -3,9 +3,7 @@ export interface PageProps {}
 
 function Page({} : PageProps) {
     return (
-        <main>
-            <h1 className="text-3xl text-[#111827] font-semibold leading-[1.2] ml-10 ">Dashboard</h1>
-        </main>
+        <main/>
     );
 }
 
